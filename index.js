b842b58c47197663b49148b4276522a446c9e5ee
rizal-mohamad <rizalmohamad209@gmail.com>
2021-03-08 20:44:20 +0700

    fix dependencies

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/", mainRoutes);
 
+console.log("====================================");
+console.log(process.env.PORT);
+console.log("====================================");
+
 app.listen(process.env.PORT || 3006, () => {
   console.log("Server Is running on port 3006");
 });
