e7b123b94c9198ef9260d2541706b36ccec2235a
rizal-mohamad <rizalmohamad209@gmail.com>
2021-03-09 23:36:45 +0700

    add public in index

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ const cors = require("cors");
 const prisma = require("@prisma/client");
 const app = express();
 const mainRoutes = require("./src/routes");
+app.use(cors());
+app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
 app.use("/", mainRoutes);
 
 app.listen(process.env.PORT || 3006, () => {
