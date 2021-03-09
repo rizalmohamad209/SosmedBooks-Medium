9027e5d431e157e7952418e43c97af71f94f24eb
rizal-mohamad <rizalmohamad209@gmail.com>
2021-03-09 21:08:07 +0700

    add cors

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 require("dotenv").config({});
 // const app = require('express')();
 const express = require("express");
+
+const cors = require("cors");
 const prisma = require("@prisma/client");
 const app = express();
 const mainRoutes = require("./src/routes");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 app.use("/", mainRoutes);
 
 console.log("====================================");
